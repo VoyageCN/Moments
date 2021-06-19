752bfa8a69eaef33846695b20ff92101f0f3f9a1
VoyageCN <ieshiaonan@gmail.com>
2021-06-19 23:42:56 +0800

    Create a new iOS project

---
+++ Moments/Moments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moments
+//
+//  Created by 史傲楠 on 2021/6/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
