a0252b1845d7c84e94b2db65f0fc2caa27b79a0a
VoyageCN <ieshiaonan@gmail.com>
2021-06-23 17:39:55 +0800

    Add models for Moments screen

---
+++ Moments/Moments/Features/Moments/Models/MomentsDetails.swift
@@ -0,0 +1,8 @@
+//
+//  MomentsDetails.swift
+//  Moments
+//
+//  Created by 史傲楠 on 2021/6/23.
+//
+
+import Foundation
---
+++ Moments/Moments/Features/Moments/Models/UserDetails.swift
@@ -0,0 +1,8 @@
+//
+//  UserDetails.swift
+//  Moments
+//
+//  Created by 史傲楠 on 2021/6/23.
+//
+
+import Foundation
