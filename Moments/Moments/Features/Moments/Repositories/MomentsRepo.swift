e56b3b40c98ac7c9c5382679111cd217075cbd07
VoyageCN <ieshiaonan@gmail.com>
2021-06-24 09:49:37 +0800

    Add

---
+++ Moments/Moments/Features/Moments/Repositories/MomentsRepo.swift
@@ -0,0 +1,30 @@
+//
+//  MomentsRepo.swift
+//  Moments
+//
+//  Created by 史傲楠 on 2021/6/24.
+//
+
+import Foundation
+import RxSwift
+
+protocol MomentsRepoType {
+    func getMoments(userID: String) -> Observable<MomentsDetails>
+}
+
+class MomentsRepo: MomentsRepoType {
+    private let getMomentsByUserIDAPISessionBuilder: () -> GetMomentsByUserIDAPISessionType
+
+    static var shared: MomentsRepo {
+        return MomentsRepo(getMomentsByUserIDAPISessionBuilder: { GetMomentsByUserIDAPISession() })
+    }
+
+    init(getMomentsByUserIDAPISessionBuilder: @escaping () -> GetMomentsByUserIDAPISessionType) {
+        self.getMomentsByUserIDAPISessionBuilder = getMomentsByUserIDAPISessionBuilder
+    }
+
+    func getMoments(userID: String) -> Observable<MomentsDetails> {
+        getMomentsByUserIDAPISessionBuilder().getMoments(userID: userID)
+    }
+
+}
