afcf77f3662a4e6b4341227806681b09651b9c75
VoyageCN <ieshiaonan@gmail.com>
2021-06-24 11:30:09 +0800

    Remove Main.storyboard and open MomentsViewController directly

---
+++ Moments/Moments/Features/Moments/Views/MomentsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MomentsViewController.swift
+//  Moments
+//
+//  Created by 史傲楠 on 2021/6/24.
+//
+
+import UIKit
+
+class MomentsViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPurple
+    }
+}
