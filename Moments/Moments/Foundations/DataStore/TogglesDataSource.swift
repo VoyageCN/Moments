a3037fce30a6b2928bd4f2b1dda3e2736f5098fd
VoyageCN <ieshiaonan@gmail.com>
2021-06-24 17:50:33 +0800

    Add

---
+++ Moments/Moments/Foundations/DataStore/TogglesDataSource.swift
@@ -0,0 +1,34 @@
+//
+//  TogglesDataSource.swift
+//  Moments
+//
+//  Created by 史傲楠 on 2021/6/24.
+//
+
+import Foundation
+
+protocol TogglesDataStoreType {
+    func isToggleOn(_ toggle: Toggle) -> Bool
+    func update(toggle: Toggle, value: Bool)
+}
+
+enum Toggle: String {
+    case isLikeButtonForMomentsEnabled
+}
+
+struct TogglesDataSotre: TogglesDataStoreType {
+    private let userDefaults: UserDefaults
+
+    init(userDefaults: UserDefaults) {
+        self.userDefaults = userDefaults
+        self.userDefaults.register(defaults: [Toggle.isLikeButtonForMomentsEnabled.rawValue: false])
+    }
+
+    func isToggleOn(_ toggle: Toggle) -> Bool {
+        return userDefaults.bool(forKey: toggle.rawValue)
+    }
+
+    func update(toggle: Toggle, value: Bool) {
+        userDefaults.set(value, forKey: toggle.rawValue)
+    }
+}
